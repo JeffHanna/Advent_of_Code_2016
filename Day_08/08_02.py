9938ad6e0960b7a2246fcd37ddb79dc4f5befb78
Jeff Hanna <jeff.b.hanna@gmail.com>
2016-12-09 17:02:51 -0600

    Day 8, both puzzles complete and correct.

--- Day_08/08_02.py
+++ Day_08/08_02.py
@@ -1,68 +1,15 @@
 # -*- coding: utf-8 -*-
 """
---- Day 8: Two-Factor Authentication ---
-You come across a door implementing what you can only assume is an 
-implementation of two-factor authentication after a long game of 
-requirements telephone.
+--- Part Two ---
+You notice that the screen is only capable of displaying capital letters; in the 
+font it uses, each letter is 5 pixels wide and 6 tall.
 
-To get past the door, you first swipe a keycard (no problem; there was one on a 
-nearby desk). Then, it displays a code on a little screen, and you type that 
-code on a keypad. Then, presumably, the door unlocks.
-
-Unfortunately, the screen has been smashed. After a few minutes, you've taken 
-everything apart and figured out how it works. Now you just have to work out 
-what the screen would have displayed.
-
-The magnetic strip on the card you swiped encodes a series of instructions for 
-the screen; these instructions are your puzzle input. The screen is 50 pixels 
-wide and 6 pixels tall, all of which start off, and is capable of three somewhat 
-peculiar operations:
-
-rect AxB turns on all of the pixels in a rectangle at the top-left of the screen 
-which is A wide and B tall.
-
-rotate row y=A by B shifts all of the pixels in row A (0 is the top row) right 
-by B pixels. Pixels that would fall off the right end appear at the left end of 
-the row.
-
-rotate column x=A by B shifts all of the pixels in column A (0 is the left 
-column) down by B pixels. Pixels that would fall off the bottom appear at the 
-top of the column.
-
-For example, here is a simple sequence on a smaller screen:
-  rect 3x2 creates a small rectangle in the top-left corner:
-  ###....
-  ###....
-  .......
-
-  rotate column x=1 by 1 rotates the second column down by one pixel:
-  #.#....
-  ###....
-  .#.....
-
-  rotate row y=0 by 4 rotates the top row right by four pixels:
-  ....#.#
-  ###....
-  .#.....
-
-  rotate column x=1 by 1 again rotates the second column down by one pixel, 
-  causing the bottom pixel to wrap back to the top:
-  .#..#.#
-  #.#....
-  .#.....
-
-As you can see, this display technology is extremely powerful, and will soon 
-dominate the tiny-code-displaying-screen market. That's what the advertisement 
-on the back of the display tries to convince you, anyway.
-
-There seems to be an intermediate check of the voltage used by the display: 
-after you swipe your card, if the screen did work, how many pixels should 
-be lit?
+After you swipe your card, what code is the screen trying to display?
 
 PERSONAL NOTES:
-* I think a Display class with 5 lists of 50 elements each and rect( ),
-  rotate_column( ), and rotate_row( ) methods may be best. It provides a
-  fairly close 1:1 mapping with the input data and naturally saves state.
+* Call Display.draw( ) after all commands have been run and 
+  look at the output. Cheap, but effective. Should I write a parse to 
+  'screen scrape' and present the result as a string?
 """
 
 import os 
@@ -98,34 +45,6 @@ class Display( object ):
 				row.append( 0 )
 
 			self._rows.append( row )
-
-
-	@property
-	def number_of_lit_pixels( self ):
-		"""		
-		A public property that walks the rows in the display and counts how many
-		pixels are on in each row. The total number of 'lit' pixels is returned
-		as the value of the property.		
-
-		**Arguments:**
-	
-			None
-	
-		**Keyword Arguments:**
-	
-			None
-	
-		**Returns:**
-	
-			`int` The number of 'lit' pixels
-		"""
-
-		num = 0
-		for r in self._rows:
-			c = [ x for x in r if x == 1 ]
-			num += len( c )
-		
-		return num
 
 
 	def draw( self ):
@@ -260,7 +179,7 @@ class Display( object ):
 				self._rows[ r ][ c ] = 1
 
 
-def get_number_of_lit_pixels( ):
+def display_message( ):
 	"""
 	Parses the input data to determine the list of draw commands that need to be 
 	run. Those commands are emulated on an instance of the Display class. 
@@ -297,14 +216,11 @@ def get_number_of_lit_pixels( ):
 				args = parts[ 1: ]
 				display.rotate( str( args[ 0 ] ), 
 									 int( args[ 1 ][ 2: ] ),
-									 int( args[ -1 ] ) )	 
-				
-			#display.draw( ) 
+									 int( args[ -1 ] ) )	  
 			
-	return display.number_of_lit_pixels
+			display.draw( )
 
 
 
 if __name__ == '__main__':
-	lit_pixels = get_number_of_lit_pixels( )
-	print( 'The number of pixels that should be lit is {0}.'.format( lit_pixels ) )
+	display_message( )
